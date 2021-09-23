8fa2b21b5103ced7cf93d7181947f8a61aa68e33
Polosmak Roman <polosmakrr@gmail.com>
2021-09-23 18:52:00 +0300

    done hw

--- app.js
+++ app.js
@@ -63,3 +63,92 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const gallery = document.querySelector('.js-gallery');
+const lightBox = document.querySelector('.js-lightbox');
+const lightImg = document.querySelector('.lightbox__image');
+
+const pictureAlbum = createImgMatrix(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', pictureAlbum);
+
+function createImgMatrix(galleryItems) {
+  return galleryItems
+    .map(({ preview, description,original}) => {
+
+      return `
+      <li class="gallery__item">
+      <a class="gallery__link" >
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+      </a>
+      </li>
+      `;
+    })
+    .join('');
+}
+
+gallery.addEventListener('click', onGalleruContainerClick);
+
+    function onGalleruContainerClick(event) {
+      
+      if (!event.target.classList.contains('gallery__image')){
+        return;
+      }
+      lightBox.classList.add('is-open')
+      lightImg.src = event.target.dataset.source;
+      lightImg.alt = event.target.alt;
+  }
+
+const closeGallery = () => {
+      lightBox.classList.remove('is-open');
+     lightImg.src = '';
+     lightImg.alt = '';
+    
+};
+lightBox.addEventListener('click', e => {
+  const target = e.target;
+  if (target.classList.contains('lightbox__button') || target.classList.contains('lightbox__overlay')) {
+    
+    closeGallery();
+  }
+    window.removeEventListener('keydown', keyClickListener);
+});
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape'|| e.key ==='Esc') {
+    closeGallery()
+  }
+})
+
+const dataSourse = [];
+const img = document.querySelectorAll('.gallery__image');
+const originalSrc = img.forEach(el => dataSourse.push(el.dataset.source));
+
+document.addEventListener('keydown', e => {
+  const currentIndex = dataSourse.indexOf(lightImg.src);
+  
+  if (e.key === 'ArrowLeft') {
+    leftClick(currentIndex);
+    } else if (e.key === 'ArrowRight') {
+    rightClick(currentIndex);
+  }
+})
+
+
+function rightClick(currentIndex) {
+  let i = currentIndex;
+  if (dataSourse.length - 1 > i) {
+    i += 1;
+    lightImg.src = dataSourse[i];
+  } else { lightImg.src = dataSourse[0]; }
+};
+function leftClick(currentIndex) {
+  let i = currentIndex;
+  if (i > 0) {
+    i -= 1;
+    lightImg.src = dataSourse[i];
+  } else {
+    i = dataSourse.length;
+    lightImg.src = dataSourse[i-1]
+  };
+}
